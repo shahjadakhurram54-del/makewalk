895d8b45ffb942dd7d2e7110d6e6cd0fce701feb
shahjadakhurram54-del <shahjadakhurram54@gmail.com>
2025-09-05 23:02:23 +0530

    Update retarget.py

--- retarget.py
+++ retarget.py
@@ -471,7 +471,7 @@ def loadRetargetSimplify(context, filepath):
     from .fkik import limbsBendPositive
 
     print("\nLoad and retarget %s" % filepath)
-    time1 = time.clock()
+    time1 = time.perf_counter()
     scn = context.scene
     trgRig = context.object
     data = changeTargetData(trgRig, scn)
@@ -595,3 +595,4 @@ def initialize():
 def uninitialize():
     for cls in classes:
         bpy.utils.unregister_class(cls)
+
